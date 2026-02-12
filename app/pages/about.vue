a7b12c4d9e4a06907b29db3664567c143159f3b3
Kent Joemar Escoto <kentescoto24@gmail.com>
2026-02-12 10:27:12 +0800

    ref and useState added

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        hi from about
-    </div>
+  <div>hi from about</div>
+
+  <div>
+    <span>
+      {{ counter }}
+    </span>
+  </div>
 </template>
+
+<script lang="ts" setup>
+const counter = useState<number>("count", () => 0);
+
+counter.value;
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,13 @@
   <div>
     <div class="text-5xl font-bold">hi from index</div>
     <NuxtLink to="/about">about</NuxtLink>
+
+    <div class="flex items-center gap-4">
+      <span>
+        {{ counter }}
+      </span>
+      <button class="px-3 py-1 bg-white text-black rounded-full" @click="counter++">Click</button>
+    </div>
   </div>
 </template>
 
@@ -9,4 +16,12 @@
 useHead({
   title: "Nuxt 4 - Test",
 });
+
+// ref()
+// const counter = ref(0);
+
+// useState()
+const counter = useState<number>('count', () => 0);
+
+counter.value;
 </script>
